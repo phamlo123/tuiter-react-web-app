71c257d3db639bc25cd1f393525b989cd5ba3ee5
phamlo123 <pham.lo@northeastern.edu>
2022-10-18 19:03:12 -0400

    more

--- src/labs/a6/classes/index.js
+++ src/labs/a6/classes/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import './index.css';
 
 function Classes() {
-    const color = 'blue';
     const dangerous = true;
     return (
       <div>
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,9 +7,9 @@ const NavigationSidebar = (
 ) => {
  return (
    <div className="list-group">
-     <a className="list-group-item">Tuiter</a>
+     <a className="list-group-item" href="/#">Tuiter</a>
      <a className={`list-group-item
-                    ${active === 'home'?'active':''}`}>
+                    ${active === 'home'?'active':''}`} href="/#">
        Home
      </a>
      <a className={`list-group-item
