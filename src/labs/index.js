149f8b833f4d64380740d84e3556ddd0b6cc5ac3
phamlo123 <pham.lo@northeastern.edu>
2022-10-18 18:46:13 -0400

    A6

---
+++ src/labs/index.js
@@ -0,0 +1,12 @@
+import Assignment6 from "./a6";
+import Nav from "../nav";
+import React from "react";
+function Labs() {
+ return (
+   <div>
+     <Nav/>
+     <Assignment6/>
+   </div>
+ );
+}
+export default Labs;
