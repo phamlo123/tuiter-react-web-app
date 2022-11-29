0d16ec0cbe21eac8858bf341672560389b8b4d96
phamlo123 <pham.lo@northeastern.edu>
2022-11-29 18:27:32 -0500

    more

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -4,26 +4,25 @@ const TUITS_API = 'https://tuiter-node-server-app-lpham.herokuapp.com/api/tuits'
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // const TUITS_API = `${API_BASE}/api/tuits`;
 
+export const createTuit = async (tuit) => {
+  const response = await axios.post(TUITS_API, tuit)
+  return response.data;
+ }
+ 
 export const findTuits = async () => {
-    const response = await axios.get(TUITS_API);
-    const tuits = response.data;
-    return tuits;
-   }   
-
+  const response = await axios.get(TUITS_API);
+  const tuits = response.data;
+  return tuits;
+}
 
 export const deleteTuit = async (tid) => {
-const response = await axios
+  const response = await axios
     .delete(`${TUITS_API}/${tid}`)
-return response.data
+  return response.data
 }
-export const createTuit = async (tuit) => {
- const response = await axios.post(TUITS_API, tuit)
- return response.data;
-}
-
 export const updateTuit = async (tuit) => {
-    const response = await axios
-      .put(`${TUITS_API}/${tuit._id}`, tuit);
-    return response.data;
-  }
+   axios
+    .put(`${TUITS_API}/${tuit._id}`, tuit);
+  return tuit;
+}
   
